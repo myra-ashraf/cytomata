6479262050d7b99372e639a49364863fe4bd4822
phuongho43 <phuongho43@gmail.com>
2017-11-11 17:36:53 -0800

    Set up module structure

---
+++ cytomata/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+    cytomatra
+    -----
+    Microenvironment simulation and control via deep reinforcement learning.
+    :copyright: (c) 2017 Phuong T. Ho
+    :license: MIT, see LICENSE for more details.
+"""
+
+
+__name__ = 'cytomata'
+__description__ = 'Microenvironment simulation and control via deep reinforcement learning.'
+__version__ = '0.1'
+__author__ = 'Phuong T. Ho'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Phuong T. Ho'
+__email__ = 'phuongho43@gmail.com'
+__keywords__ = 'cytomata deep-learning reinforcement-learning research cells engineering'
+__website__ = 'https://github.com/phuongho43/cytomata'
+
+
+from cytomata import simulator
+from cytomata import controller
