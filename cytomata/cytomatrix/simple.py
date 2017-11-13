070e6280efca92acb4bb73442ec20a4c666a9ac0
phuongho43 <phuongho43@gmail.com>
2017-11-12 17:33:43 -0800

    Set up settings and sprites

--- cytomata/cytomatrix/simple.py
+++ cytomata/cytomatrix/simple.py
@@ -1,76 +1,92 @@
+import sys
 import pygame as pg
-import random as rnd
+from settings import *
+from sprites import *
 
 
-# Settings
-########################
-TITLE = "Cytomata"
-WIDTH = 360
-HEIGHT = 480
-FPS = 30
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-BLUE = (25,107,189)
-RED = (194, 40, 40)
-GREEN = (54,143,59)
-########################
-
-
-class Game():
+class Game:
     """Game manager"""
     def __init__(self):
-        # initialize game window, etc.
+        """Initialize game, window, etc."""
         pg.init()
-        pg.mixer.init()
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
-        self.running = True
+        pg.key.set_repeat(500, 100)
+        self.load_data()
+
+    def load_data(self):
+        pass
 
     def new(self):
-        # start a new game
+        """Set up vars/objects for new game"""
         self.all_sprites = pg.sprite.Group()
-        self.run()
+        self.walls = pg.sprite.Group()
+        self.player = Player(self, 10, 10)
+        for x in range(10, 20):
+            Wall(self, x, 5)
 
     def run(self):
-        # game loop
+        """Game loop"""
         self.playing = True
         while self.playing:
-            self.clock.tick(FPS)
+            self.dt = self.clock.tick(FPS) / 1000
             self.events()
             self.update()
             self.draw()
 
-    def events(self):
-        # game loop - processing
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                if self.playing:
-                    self.playing = False
-                self.running = False
+    def quit(self):
+        """Quit to desktop"""
+        pg.quit()
+        sys.exit()
 
     def update(self):
-        # game loop - updates
+        """Game loop - updates"""
         self.all_sprites.update()
 
+    def draw_grid(self):
+        """Draw the tiles based on settings"""
+        for x in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+
     def draw(self):
-        # game loop -rendering
-        self.screen.fill(BLACK)
+        """Game loop - render"""
+        self.screen.fill(BGCOLOR)
+        self.draw_grid()
         self.all_sprites.draw(self.screen)
         pg.display.flip()
 
+    def events(self):
+        """Game loop - process inputs/events"""
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    self.quit()
+                if event.key == pg.K_LEFT:
+                    self.player.move(dx=-1)
+                if event.key == pg.K_RIGHT:
+                    self.player.move(dx=1)
+                if event.key == pg.K_UP:
+                    self.player.move(dy=-1)
+                if event.key == pg.K_DOWN:
+                    self.player.move(dy=1)
+
     def show_start_screen(self):
-        # game start screen
+        """Game start screen"""
         pass
 
-    def show_gover_screen(self):
-        # game over screen
+    def show_go_screen(self):
+        """Game over screen"""
         pass
 
+# Executed code
 g = Game()
 g.show_start_screen()
-while g.running:
+while True:
     g.new()
-    g.show_gover_screen()
-
-pg.quit()
+    g.run()
+    g.show_go_screen()
