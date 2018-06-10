f00ab4af79d3507cf0e993f4c147092469e77881
Phuong T. Ho <phuongho43@gmail.com>
2018-06-10 14:58:26 -0700

    Get step function experiment to work

--- in_vitro.py
+++ in_vitro.py
@@ -10,41 +10,8 @@ from cytomata.control import BangBang, PID
 from cytomata.interface import Microscope
 
 
-def no_light(save_dir, t_total=21600, img_int=600, ch_dark='None'):
-    """Negative control for microscope light induction experiments.
-
-    No light stimulus is applied but images are still taken at regular time
-    intervals via the microscope camera and processed to calculate fluorescence
-    intensity as the output. At each timepoint, the input (light intensity = 0
-    in this case) and output (fluorescence intensity) are logged as csv files
-    and images taken are saved as well.
-
-    Args:
-        save_dir: File directory to save data for this experiment.
-        t_total (seconds): How long the enire experiment will last.
-        img_int (seconds): Duration of time before repeating script.
-        ch_dark (MM config): Micromanager channel of the dark state.
-    """
-    mic = Microscope()
-    mic.set_channel(ch_dark)
-    data = []
-
-    def tasks():
-        img = mic.take_snapshot()
-        roi_int, roi, bg_int, bg = mic.measure_fluorescence(img)
-        data.append([roi_int, bg_int])
-
-    schedule.every(img_int).seconds.do(tasks)
-    t0 = time.time()
-    while time.time() - t0 < t_total:
-        schedule.run_pending()
-
-    np.savetxt('no_light.csv', np.array(data), delimiter=',',
-        header='fluo_intensity, bkg_intensity', comments='')
-
-
-def step_up_down(save_dir, t_total=21600, img_int=600, t_on=7200,
-    t_off=14400, ch_dark='None', chs_img=['GFP', 'mCherry'],
+def step_up_down(save_dir, t_total=10800, img_int=300, t_on=600,
+    t_off=10800, ch_dark='None', chs_img=['GFP', 'mCherry'],
     ch_exc='Induction-460nm'):
     """Use step functions to characterize an optogenetic system.
 
@@ -65,7 +32,12 @@ def step_up_down(save_dir, t_total=21600, img_int=600, t_on=7200,
         chs_img (MM config): Micromanager channels for taking images.
         ch_exc (MM config): Micromanager channel of induction light.
     """
-    os.makedirs(save_dir, exist_ok=True)
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    img_dirs = [os.path.join(save_dir, 'imgs', ch) for ch in chs_img]
+    for d in img_dirs:
+        if not os.path.exists(d):
+            os.makedirs(d)
 
     mic = Microscope()
     mic.set_channel(ch_dark)
@@ -81,24 +53,22 @@ def step_up_down(save_dir, t_total=21600, img_int=600, t_on=7200,
                 save_dir, 'imgs', ch, str(round(ts)) + '.png')
             cv2.imwrite(img_path, img)
             roi_int, roi, bg_int, bg = mic.measure_fluorescence(img)
-            d.append(roi_int, bg_int)
+            d += [roi_int, bg_int]
+        if (time.time() >= t0 + t_on and time.time() <= t0 + t_off):
+            mic.set_channel(ch_exc)
+        else:
+            mic.set_channel(ch_dark)
         data.append([ts] + d)
+        data_path = os.path.join(save_dir, 'step_up_down.csv')
+        column_names = ', '.join(['time (s)'] + [
+            ch + d for ch in chs_img for d in ['_fluo_int', '_bg_int']])
+        np.savetxt(data_path, np.array(data), delimiter=',',
+            header=column_names, comments='')
 
     schedule.every(img_int).seconds.do(record_data)
     t0 = time.time()
     while time.time() - t0 < t_total:
         schedule.run_pending()
-        if (time.time() > t0 + t_on and time.time() < t0 + t_off
-            and mic.get_channel() != ch_exc):
-            mic.set_channel(ch_exc)
-        else:
-            mic.set_channel(ch_dark)
-
-    data_path = os.path.join(save_dir, 'step_up_down.csv')
-    column_names = ', '.join(['time (s)'] + [
-        ch + d for ch in chs_img for d in ['_fluo_int', '_bg_int']])
-    np.savetxt(data_path, np.array(data), delimiter=',',
-        header=column_names, comments='')
 
 
 if __name__ == '__main__':
